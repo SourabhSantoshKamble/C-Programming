77a0b3afc009cfa292ff04f18a700d7b4e9da0ac
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-02 14:58:19 +0530

    Add Reverse Character code

---
+++ Basic/Reverse Characters/ReverseCharacters.c
@@ -0,0 +1,13 @@
+// C Program to print reverse characters
+
+# include <stdio.h>                                                                 // Header file declaration
+
+int main () {                                                                       // main function definition
+
+    char one = 'X';
+    char two = 'M';
+    char three = 'L';
+    printf("\n The reverse of %c%c%c is %c%c%c\n\n",one,two,three,three,two,one);   // Printing reverse 
+    return 0;
+
+}                                                                                   // End of main function
