ea42c4e7d24b54e38848948b0d84829030e5e07e
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-02 13:05:58 +0530

    Add HelloWorld code

---
+++ Basic/Hello World/HelloWorld.c
@@ -0,0 +1,10 @@
+// C Program to print the statement Hello World !
+
+# include <stdio.h>                                                                             // Header file declaration
+
+int main () {                                                                                   // main function definition
+
+    printf("\n Hello World!\n\n");                                                              // Printing required statement
+    return 0;
+
+}                                                                                               // End of main function
