d1f147d5e7f216064f63af7d824e4fdf74bfa4ca
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-02 13:27:45 +0530

    Create Print Details folder

---
+++ Basic/Print Details/PrintDetails.c
@@ -0,0 +1,12 @@
+// C Program to Print the details such as Name, DOB, and mobile number
+
+# include <stdio.h>                                                                     // Header file declaration
+
+int main () {                                                                           // main function definition
+
+    printf("\n Name : SSK\n");                                                          // Printing required details
+    printf("\n DOB : 05 - Jan - 1950\n");
+    printf("\n Mobile number : +91-9568123401\n\n");
+    return 0; 
+
+}                                                                                       // End of main function
