e92d743a86c569e51e65b72702b6fc1ed3a8e50c
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-02 14:49:09 +0530

    Add Print Block code

---
+++ Basic/Print Block/PrintBlock.c
@@ -0,0 +1,33 @@
+// C Program to build a block of Letter
+
+# include <stdio.h>                                                         // Header file declaration
+
+int main () {                                                               // main function definition
+
+    // Code snippet to print F Block
+    
+    printf("\n#######");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#####");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n\n");
+
+    // Code snippet ot print C Block
+    
+    printf("\n  #####");
+    printf("\n #     #");
+    printf("\n#       #");
+    printf("\n#");
+    printf("\n#");                                                                       // End of for
+    printf("\n#");
+    printf("\n#       #");
+    printf("\n #     #");
+    printf("\n  #####");
+    printf("\n\n");
+
+    return 0;
+
+}                                                                           // End of main function
