ecf3c0fe4966e5e58205c99a9f95ab046c8418bc
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-02 13:29:48 +0530

    Add Print details code

--- Basic/Print Details/PrintDetails.c
+++ Basic/Print Details/PrintDetails.c
@@ -7,6 +7,6 @@ int main () {                                                                   
     printf("\n Name : SSK\n");                                                          // Printing required details
     printf("\n DOB : 05 - Jan - 1950\n");
     printf("\n Mobile number : +91-9568123401\n\n");
-    return 0; 
+    return 0;
 
 }                                                                                       // End of main function
