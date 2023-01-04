c7867d1a49eb71f4ad612e760d58b884233b3d72
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-04 20:52:39 +0530

    Add Divisor folder

---
+++ Basic/Divisor/Divisor.c
@@ -0,0 +1,20 @@
+// C Program to find the Divisor's of a entered integer
+
+# include <stdio.h>                                                                 // Header file declaration
+
+int main () {                                                                       // main function definition
+
+    int num;                                                                        // Variable declaration
+    printf("\n Enter an integer to find its divisor's : ");
+    scanf("%i",&num);                                                               // User input number
+    printf("\n The divisors of %i are \n\n",num);
+    for (int i = 1;i <= num;i++){                                                   // for logic for iteration
+
+        if (num%i == 0)
+            printf("%i ",i);                                                        // Printing divisors    
+
+    }                                                                               // End of for
+    printf("\n\n");
+    return 0;
+
+}                                                                                   // End of main function
