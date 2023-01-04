b1507481c39915bfcad060383de5f42971d217c5
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-01-04 20:46:21 +0530

    Add s code

---
+++ Basic/s/s.c
@@ -0,0 +1,11 @@
+// C Program to find the value of s where s = 1 + 3/2 + 5/4 + 7/8
+
+# include <stdio.h>                                                                         // Header file declaration
+
+int main () {                                                                               // main function definition
+
+    float s = 1 + (float)3/2 + (float)5/4 + (float)7/8;                                     // Variable initialization
+    printf("\n s : %0.5f\n\n",s);
+    return 0;
+
+}                                                                                           // End of main function
